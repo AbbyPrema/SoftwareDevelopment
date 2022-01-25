17cd6a0b47763f19a55dbc69049e66db7fd5c1c5
AbbyPrema <premacab@sheridancollege.ca>
2022-01-25 00:50:50 -0500

    my first commit

---
+++ src/personlist/Person.java
@@ -0,0 +1,36 @@
+
+package personlist;
+
+
+public class Person {
+      private String personName;
+    private String lastName;
+
+    /**
+     * @return the personName
+     */
+    public String getPersonName() {
+        return personName;
+    }
+
+    /**
+     * @param personName the personName to set
+     */
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/personlist/PersonList.java
@@ -0,0 +1,21 @@
+package personlist;
+
+import java.util.Scanner;
+
+
+public class PersonList {
+
+   
+    public static void main(String[] args) {
+         
+        Scanner input = new Scanner(System.in);
+        
+        Person[] p=new Person[3];
+        for (int i=0;i>p.length;i++){
+            System.out.println("insert person");
+        
+            p[i].setPersonName(input.nextLine());
+    }
+    }
+    }
+
